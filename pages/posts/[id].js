cbf7ee250fcf2eb1893c659ffb9e94c37f645867
Sarah Mattar <sarah.mattar@mac.com>
2023-02-21 17:25:34 -0800

    Update imports

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Layout from '../../components/layout';
+import Layout from '../../components/Layout';
 import Date from '../../components/Date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
